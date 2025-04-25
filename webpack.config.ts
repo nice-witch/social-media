0ed9ef0917f0dd05829bc764bd7800a997067d55
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-04-25 17:08:07 +0500

    fix: refactoring config file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,25 @@
-import path from 'path'
 import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import {buildWebpackConfig} from "./config/build/BuildWebpackConfig";
+import path from "path";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry : path.resolve(__dirname, 'src', 'index.ts'),
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output : {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins : [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
-        }),
-    ]
-}
 
-export default config;
+export default (env: BuildEnv) => {
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT,
+    });
+
+    return config;
+};
