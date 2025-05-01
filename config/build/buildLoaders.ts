57e553be0266103cd4abf40b7058b1b3cbb1efc9
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-05-01 11:36:12 +0500

    add: config for module style

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,8 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders():webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
     const typeScriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -10,8 +12,16 @@ export function buildLoaders():webpack.RuleSetRule[] {
     const styleLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            "style-loader",
-            "css-loader",
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: ((resourcePath: string) => resourcePath.includes('.modules.')),
+                        localIdentName: isDev ? '[path][name]__[local]' : "[hash:base64]",
+                    }
+                },
+            },
             "sass-loader",
         ],
     };
