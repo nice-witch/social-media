222a20dd7ecaa956d2a708d3d6633fcba9ddaa6c
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-13 14:00:03 +0500

    add: navbar in app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,16 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from "Shared/lib/classNames/classNames";
 import {useTheme} from "App/providers/themeProvider";
 import {AppRoute} from "App/providers/routes";
-
+import {Navbar} from "Widgets/Navbar";
 
 function App(){
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>toggle theme</button>
-            <Link to="/">Главная</Link>
-            <Link to="/about">О нас</Link>
             <AppRoute />
         </div>
 )
