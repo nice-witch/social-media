3de8fe6b6c454bddbd829a6b0229cbd3470e3091
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-13 14:02:15 +0500

    fix: add default variables

--- src/Shared/lib/classNames/classNames.tsx
+++ src/Shared/lib/classNames/classNames.tsx
@@ -1,13 +1,11 @@
 type Mods = Record<string, string | boolean>
 
-export function classNames(cls: string, mods: Mods, additionally: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additionally: string[] = []): string {
     return [
         cls,
-        ...additionally,
+        ...additionally.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className, value]) => className)
     ].join(' ');
 }
-
-// { active: true, focus: false }
