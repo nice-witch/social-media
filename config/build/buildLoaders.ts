dcc9bfeb4b424b19cfcf2e4359d3cc0f52c7bd4e
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-14 12:47:00 +0500

    add: loader for images file

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -26,8 +26,24 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
         ],
     };
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
     return [
         typeScriptLoader,
         styleLoader,
+        svgLoader,
+        fileLoader,
     ]
 }
