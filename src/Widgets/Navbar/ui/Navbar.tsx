629688ea9a04c9ff9ccb315db849f66d45ac3749
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-14 14:20:05 +0500

    delete: remove theme switcher from navbar

--- src/Widgets/Navbar/ui/Navbar.tsx
+++ src/Widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import {classNames} from "Shared/lib/classNames/classNames";
 import {MyLink} from "Shared/ui/MyLink/MyLink";
 import cls from './Navbar.module.scss';
-import {ThemeSwitcher} from "Widgets/ThemeSwitcher";
 
 interface navbarProps {
     className?: string;
@@ -11,7 +10,6 @@ export const Navbar = ({className}: navbarProps) => {
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <ThemeSwitcher />
             <div className={classNames(cls.linkContainer)}>
                 <MyLink to="/">Главная</MyLink>
                 <MyLink to="/about">О нас</MyLink>
