5294bd2ef88bc79dacc72f5b0a9a0c516cce011e
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-14 13:26:26 +0500

    fix: export theme

--- src/App/providers/themeProvider/index.ts
+++ src/App/providers/themeProvider/index.ts
@@ -1,7 +1,9 @@
 import {useTheme} from "./lib/useTheme";
 import ThemeProvider from "./ui/themeProvider";
+import {Theme} from "./lib/themeContext";
 
 export {
     useTheme,
     ThemeProvider,
+    Theme,
 }
