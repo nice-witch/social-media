c0315fdfba463745927d24d8e70059c9b3b9f6a1
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-14 13:25:50 +0500

    fix: add theme switcher in navbar

--- src/Widgets/Navbar/ui/Navbar.tsx
+++ src/Widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import {classNames} from "Shared/lib/classNames/classNames";
 import {MyLink} from "Shared/ui/MyLink/MyLink";
 import cls from './Navbar.module.scss';
+import {ThemeSwitcher} from "Widgets/ThemeSwitcher";
 
 interface navbarProps {
     className?: string;
@@ -10,7 +11,7 @@ export const Navbar = ({className}: navbarProps) => {
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div>logo</div>
+            <ThemeSwitcher />
             <div className={classNames(cls.linkContainer)}>
                 <MyLink to="/">Главная</MyLink>
                 <MyLink to="/about">О нас</MyLink>
