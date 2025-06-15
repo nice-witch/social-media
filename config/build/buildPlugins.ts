cb40e36137f8ebeab2cf0559ac06a63ede4201c4
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-15 12:04:53 +0500

    add(config): add global variable ID_DEV

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,7 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins(htmlPath:string): webpack.ProgressPlugin[] {
+export function buildPlugins(htmlPath:string, isDev: boolean): webpack.ProgressPlugin[] {
     return [
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
@@ -11,6 +11,9 @@ export function buildPlugins(htmlPath:string): webpack.ProgressPlugin[] {
         new MiniCssExtractPlugin({
             filename: "css/[name].[contenthash:8].css",
             chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
         })
     ]
 }
