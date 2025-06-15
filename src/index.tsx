cd15a8430b312c18a6d91c1d33af502be5ce5ffd
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-15 13:23:06 +0500

    add(i18n): add i18n in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import {render} from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App/App";
 import {ThemeProvider} from "App/providers/themeProvider";
+import './Shared/config/i18n';
 
 render(
     <BrowserRouter>
