c1cbb6fbcfdb9597970ff19cecf0ef5d63b4b7db
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-16 10:48:11 +0500

    add: HotModuleReplacementPlugin, ReactRefreshPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins(htmlPath:string, isDev: boolean): webpack.ProgressPlugin[] {
     return [
@@ -14,6 +15,8 @@ export function buildPlugins(htmlPath:string, isDev: boolean): webpack.ProgressP
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        isDev && (new ReactRefreshPlugin()),
+        isDev && (new webpack.HotModuleReplacementPlugin()),
     ]
 }
