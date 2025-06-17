ddea3fb99e36a053676e26b992e923826f06f2a9
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-17 11:29:45 +0500

    add: stylelint, script and rules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('stylelint').Config} */
 export default {
-    extends: ["stylelint-config-standard-scss"]
+    extends: ["stylelint-config-standard-scss"],
+    rules: {
+        "selector-class-pattern": ["^[a-z][a-zA-Z0-9]*$", {
+            "message": "Классы должны быть в стиле camelCase (например, langSwitcher)"
+        }]
+    }
 };
