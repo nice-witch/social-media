402bb91c630da72e9aab58793a6eeb77ee67986a
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-19 12:14:40 +0500

    add not-found route

--- src/Shared/config/RouteConfig.tsx
+++ src/Shared/config/RouteConfig.tsx
@@ -1,15 +1,18 @@
 import {RouteProps} from "react-router-dom";
 import {MainPage} from "Pages/MainPage";
 import {AboutPage} from "Pages/AboutPage";
+import {NotFoundPage} from "Pages/NotFoundPage";
 
 enum Pages {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 const pagesPath: Record<Pages, string> = {
     [Pages.MAIN]: '/',
     [Pages.ABOUT]: '/about',
+    [Pages.NOT_FOUND]: '/*',
 }
 
 export const routeConfig: Record<Pages, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<Pages, RouteProps> = {
     [Pages.ABOUT]: {
         path: pagesPath.about,
         element: <AboutPage />
+    },
+    [Pages.NOT_FOUND]: {
+        path: pagesPath.not_found,
+        element: <NotFoundPage />
     },
 }
