0d8e6ac87e93c8af97d08d856e09bfc83305f6d4
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-06-28 14:57:19 +0500

    fiz babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
+        [
+            '@babel/preset-env',
+            {targets: {node: 'current'},}
+        ],
         '@babel/preset-typescript',
-        '@babel/preset-react'
+        ['@babel/preset-react', {runtime: "automatic"}]
     ],
 };
