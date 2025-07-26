839e2e2515f19730e287fbfadf065f583ec431bf
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-07-26 19:57:39 +0500

    add: new theme for button

--- src/Shared/ui/Button/Button.tsx
+++ src/Shared/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    OUTLINE = 'outline',
 }
 
 interface buttonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
