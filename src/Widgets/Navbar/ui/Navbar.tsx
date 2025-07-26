94577b19533ed2344bfd3ce78eb958bff793c708
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-07-26 19:59:32 +0500

    fix: add translate

--- src/Widgets/Navbar/ui/Navbar.tsx
+++ src/Widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,19 @@
 import {classNames} from "Shared/lib/classNames/classNames";
 import {MyLink} from "Shared/ui/MyLink/MyLink";
 import cls from './Navbar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface navbarProps {
     className?: string;
 }
 
 export const Navbar = ({className}: navbarProps) => {
-
+    const { t } = useTranslation();
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <div className={classNames(cls.linkContainer)}>
-                <MyLink to="/">Главная</MyLink>
-                <MyLink to="/about">О нас</MyLink>
+                <MyLink to="/">{t('Главная')}</MyLink>
+                <MyLink to="/about">{t('О сайте')}</MyLink>
             </div>
         </div>
     );
