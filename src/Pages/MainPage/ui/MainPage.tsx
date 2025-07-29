88f04325251cf0d703328fd4de8304a86b14091c
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-07-29 13:48:06 +0500

    fix chromatics scripts

--- src/Pages/MainPage/ui/MainPage.tsx
+++ src/Pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
-import {BugButton} from "App/providers/errorBoundaries";
 
 function MainPage() {
     const {t} = useTranslation('main');
